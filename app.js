4c1d711e1ca40977019ee651e0441ec1c7ad79f0
innaslota <innaslota8@gmail.com>
2023-01-20 19:42:51 +0200

    Added Project page and styled it. Added swiper for the mobile mode

--- app.js
+++ app.js
@@ -50,3 +50,26 @@ tabs.forEach((tab) => {
     tab.classList.add("qualification-active");
   });
 });
+
+//project swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
